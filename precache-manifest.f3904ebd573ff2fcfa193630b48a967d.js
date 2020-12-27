e89c2e2f08da51a5cb1b4f097ad8bce0bf432ebc
shivam jadhav <jadhavshivam0228@gmail.com>
2020-12-27 18:15:55 +0530

    Updates

---
+++ precache-manifest.f3904ebd573ff2fcfa193630b48a967d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930e8bc30e7876f892b9ae6f3f5f17ce",
+    "url": "/My_shopping_Cart/index.html"
+  },
+  {
+    "revision": "8b2a69536a55bdd7673e",
+    "url": "/My_shopping_Cart/static/css/main.9856c331.chunk.css"
+  },
+  {
+    "revision": "82b54e1ef7ee521856fa",
+    "url": "/My_shopping_Cart/static/js/2.7f58288d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My_shopping_Cart/static/js/2.7f58288d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b2a69536a55bdd7673e",
+    "url": "/My_shopping_Cart/static/js/main.fe7fc768.chunk.js"
+  },
+  {
+    "revision": "2c7c9d04085223a802e6",
+    "url": "/My_shopping_Cart/static/js/runtime-main.eb4953be.js"
+  }
+]);
